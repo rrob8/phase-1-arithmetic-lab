f4dba722cb2d86e9c88714c16a75a70970280107
Robert Bubnik <rbubnik@gmail.com>
2024-09-13 16:24:12 -0400

    passing

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 12
+let num4 = 3
+
+let mod = num3/num4
+
+let max = Math.max(1,5,12,20)
